6ebe7da56912f0cd136934e44badf38c5b1896b7
Julian Beltran <cesarbel@hotmail.com>
2021-03-21 17:24:45 -0400

    connection configuration (production)

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '123',
-    database : 'smart-brain'
+  	connectionString : process.env.DATABASE_URL,
+  	ssl: true
+    //host : '127.0.0.1',
+    //password : '123',
+    //user : 'postgres',
+    //database : 'smart-brain'
   }
 });
 
@@ -32,6 +34,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
 })
